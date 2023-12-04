1243e476615b37872f29546893028c871463ab4a
youda <yudahyeon2@naver.com>
2023-12-04 17:45:46 +0900

    필요없는 코드 삭제

--- gui/enter_the_eqation.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-def enter_the_equation(root,value):
-    label = tk.Label(root,text=f"{value}: ")
-    label.grid(row=0, column=0, padx=10, pady=10, sticky="E")
-    entry = tk.Entry(root)
-    entry.grid(row=0, column=1, padx=10, pady=10)
-    return entry
